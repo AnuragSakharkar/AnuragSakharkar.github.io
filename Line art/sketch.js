e6f47b4ec617495e968383972e552075b8ae8438
Anurag <anurag.kishore202@gmail.com>
2019-09-09 14:11:16 -0600

    Not much

--- Line art/sketch.js
+++ Line art/sketch.js
@@ -1,39 +1,16 @@
-// Project Title
-// Your Name
-// Date
+// Line art
+// Anurag Sakharkar
+// 9 Sept 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let redAmount = 0;
-let blueAmount = 0;
-let greenAmount = 0;
-
-let rChange = 3;
-let gChange = 2;
-let bChange = 1;
-
 function setup() {
+  frameRate(120);
   createCanvas(windowWidth, windowHeight);
-  background(125);
+  background(255);
 }
 
 function draw() {
-  noStroke();
-  fill(redAmount, greenAmount, blueAmount);
-  ellipse(mouseX, mouseY, 100 , 100);
-
-  redAmount = redAmount + rChange
-  greenAmount = greenAmount + gChange
-  blueAmount = blueAmount + bChange
-
-  if (redAmount > 255 || redAmount < 0){
-    rChange = -rChange;
-  }
-  if (greenAmount > 255 || greenAmount < 0){
-    gChange = -gChange;
-  }
-  if (blueAmount > 255 || blueAmount < 0){
-    bChange = -bChange;
-  }
+  line(mouseX,mouseY,pmouseX,pmouseY)
 }
