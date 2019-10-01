e9626befc9ad8e24eae7b3408494c5110cd9a064
Anurag <anurag.kishore202@gmail.com>
2019-10-01 14:11:38 -0600

    Add image manipulation demo

--- Class demos/4. Image manipulation/sketch.js
+++ Class demos/4. Image manipulation/sketch.js
@@ -1,130 +1,55 @@
-// Bouncing logo
+let mountainImage
+let filteredImage
+
+// Image Manipulation demo
 // Anurag Sakharkar
 // 9 Sept 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x;
-let y;
-let dx = 10;
-let dy = 10;
-let radius;
-let rectsize;
-let state = "menu";
+function preload(){
+  mountainImage = loadImage("assets/Mountains.jpg")
+}
 
 
-function setup() 
-{
-
+function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  y = height/2;
-  dx = random(-100, 100);
-  dy = random(-100, 100);
-  radius = 100;
-  rectsize = 50;
-
+  filteredImage = RGBspotlight(mountainImage);
 }
 
-function draw() 
-{
 
-  background(255);
-
-
-  if (state === "menu") 
-  {
-  showMenu();
-  checkIfButtonClicked();
-  }
-    
-
-
-  else if (state === "circle") 
-  {
-    displayCircle();
-    x += dx;
-    y += dy;
-  }
-
-
-  else if (state === "rectangle")
-  {
-    displayRectangle(); 
-    x += dx;
-    y += dy;
-  }
-
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-
+function draw() {
+  background(0);
+  image(filteredImage, 0, 0);
 }
 
 
 
-function displayCircle()
-{
+function RGBspotlight(sourceImage) {
 
-  if (x > windowWidth - radius/2 || x < 0 + radius/2){
-    dx *= -1
-  }
+  let img = createImage(sourceImage.width, sourceImage.height);
+  img.loadPixels();
+  sourceImage.loadPixels();
 
-  if (y > windowHeight - radius/2 || y < 0 + radius/2){
-    dy *= -1
-  }
+  for (let x = 0; x < sourceImage.width; x++) {
+    for (let y = 0; y < sourceImage.height; y++) {
+      let p = sourceImage.get(x, y);
 
-  fill(0);
-  circle(x, y, radius);
+      let r = red(p);
+      let g = green(p);
+      let b = blue(p);
 
-}
+      let newValues = ((r+g+b)/3)
 
-
-function displayRectangle() 
-{
-
-  if (x > windowWidth - rectsize || x < 0){
-    dx *= -1
-  }
-
-  if (y > windowHeight - rectsize || y < 0){
-    dy *= -1
-  }
-
-  fill(0);
-  rect(x, y, rectsize, rectsize);
-
-}
-
-
-function showMenu() {
-
-  rectMode(CENTER);
-  rect(width/2, height/2,-100, 200, 75)
-  textAlign(CENTER, CENTER);
-  textSize(50);
-  text("RECTANGLE", width/2, height/2 - 100)
-
-
-  rect(width/2, height/2,+100, 200, 75)
-  textAlign(CENTER, CENTER);
-  textSize(50);
-  text("RECTANGLE", width/2, height/2 + 100)
-
-}
-
-function checkIfButtonClicked() {
-  if (mouseIsPressed) {
-    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 - 175 && mouseY < height/2 + 75) {
-      state = "rectangle";
-    }
-    if (mouseX > width/2 - 200 && mouseX < width/2 + 200 && mouseY > height/2 - 75 && mouseY < height/2 - 25) {
-      state = "rectangle";
+      if (dist(mouseX, mouseY, x, y) > 100){
+        img.set(x, y, color(newValues, newValues, newValues));
+      }
+      else {
+        img.set(x, y, color(r, g, b));
+      }
     }
   }
-
-
-
+  img.updatePixels();
+  return img;
 }
